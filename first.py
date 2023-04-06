1566d03ee006931eecda52561f80a29ce0aa5362
neuro <yamsh@yandex.ru>
2023-04-06 14:24:45 +0600

    Function, variables, dictionaries, user input, libraries, lists, methods

--- first.py
+++ first.py
@@ -1,3 +1,11 @@
-name = input ("What is you name? ")
-print(name)
+import random
+
+def get_choices():
+    player_choice = input("Enter a choice (rock, paper, scissors): ")
+    options = ["rock", "paper", "scissors"]
+    computer_choice = random.choice(options)
+    choices = {"player" : player_choice, "computer" : computer_choice}
+    return choices
+results = get_choices()
+print(results)
 
