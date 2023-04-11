5e058587241435835dbb4a63de0c047a2ce5c1a8
neuro <yamsh@yandex.ru>
2023-04-11 17:08:37 +0600

    tuples

---
+++ tuples.py
@@ -0,0 +1,10 @@
+# tuples
+names = ("Roger", "Syd", "Ken")
+
+print(names[0]) # indexing
+print(names.index("Roger")) # index method, print index number
+print(len(names))
+print("Roger" in names)
+print(sorted(names)) # creates new sorted list
+newTuple = names + ('Ross', 'Hugo') # creating new tuple, combining two 
+print(newTuple)
