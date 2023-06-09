9ea9799149b170555445535bb4122eb075ef96af
neuro <yamsh@yandex.ru>
2023-06-09 15:16:02 +0600

    string methods and f-string

--- ch2_variables/methods.py
+++ ch2_variables/methods.py
@@ -1,2 +1,11 @@
 name = 'ada moltpensen'
 print(name.title())
+print(name.upper())
+print(name.lower())
+# print(dir(name)) to print all available methods
+first_name = 'michael'
+second_name = 'boumon'
+full_name = f'{first_name} {second_name}'
+print(f"Hello, {full_name.title()}!\nor")
+message = f'This will be your gide {name.title()}'
+print(message)
