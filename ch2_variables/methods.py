796ce59d1c1ac3b46cc1e139ab3f71551679f4fb
neuro <yamsh@yandex.ru>
2023-06-09 15:58:44 +0600

    .strip() .removeprefix() .removesuffix() methods

--- ch2_variables/methods.py
+++ ch2_variables/methods.py
@@ -9,3 +9,10 @@ full_name = f'{first_name} {second_name}'
 print(f"Hello, {full_name.title()}!\nor")
 message = f'This will be your gide {name.title()}'
 print(message)
+favorite_language = '   python '
+print(favorite_language.strip())
+google_url = 'https://google.com'
+print(google_url.removeprefix('https://'))
+filename = 'python_notes.txt'
+removesuf = filename.removesuffix('.txt')
+print(removesuf)
