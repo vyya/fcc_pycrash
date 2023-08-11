dcded8f2f87b0eb9d6a936a5fb283359d05a7a3e
neuro <yamsh@yandex.ru>
2023-08-11 16:56:09 +0600

    incomplete version

--- ch3_lists/ar_form.py
+++ ch3_lists/ar_form.py
@@ -1,14 +1,43 @@
-# expressions = []
-def arifm_arrange(lst):
-    for item in lst:
-         exp = eval(item)
-         piece = item.split(' ')
-         for crumble in piece:
-                print(crumble)
-                
-         print('---')
-         print(exp)        
-        
-# user_input = input("Enter ariphmetic expression separated by comma: ")
-# exp = user_input.split(',')
-arifm_arrange(['45 + 25', '34 - 67'])
+def arithm_arrange(lst):
+    if len(lst) > 15:
+              print('Error: Too many problems')
+    else:
+       operand1 = []
+       operand2 = []
+       operator = []
+       len_max = []
+       for item in lst:
+              #print(item)
+              #exp = eval(item)
+              piece = item.split(' ')
+              #print(piece)             
+              #operand1.append(piece[0])
+              #operand2.append(piece[2])
+              #operator.append(piece[1])
+              lst_i = []
+              for i in piece:
+                     #length.append(len(i))
+                     #print(i)
+                     lst_i.append(i)
+                     print(lst_i)
+              
+              #print(operand1)
+              #print(operand2)
+              #print(operator)
+              #return piece
+'''def max_length(piece):
+      
+       print(length)
+
+              #for crumble in piece:
+                     #operand1.append(crumble)
+                     #print(operand1)                     
+              #print('---')
+              #print(exp)
+def main():        
+       piece = arithm_arrange(['4569 + 25', '3476 - 67', '45 + 8'])
+       max_length(piece)
+
+main() 
+'''    
+arithm_arrange(['4569 + 25', '3476 - 67', '45 + 8'])
