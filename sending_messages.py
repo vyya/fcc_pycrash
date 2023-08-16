0566cca5db9e1bff952e9d01fbc71e3bc9a47d89
neuro <yamsh@yandex.ru>
2023-08-16 19:01:43 +0600

    function that moves one list to another

---
+++ sending_messages.py
@@ -0,0 +1,11 @@
+def send_messages(messages, sent_messages):
+        while messages:
+                outbox = messages.pop()
+                sent_messages.append(outbox)
+        print(messages)
+        print(sent_messages)
+            
+
+messages = ['Hi there!', 'Look out!', 'Morning sir', 'Astalavista baby']
+sent_messages = []
+send_messages(messages, sent_messages)
