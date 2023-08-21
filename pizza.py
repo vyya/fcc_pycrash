fecf4341c68f6a1ce7d95baa60ea2879e7f335dd
neuro <yamsh@yandex.ru>
2023-08-21 22:58:14 +0600

    positional parameters added to arbitrary parameters

--- pizza.py
+++ pizza.py
@@ -1,7 +1,9 @@
-def make_pizza(*toppings):
+def make_pizza(size, *toppings):
     """Print a list of toppings that have been requested"""
     print(f'Here is the list of the toppings have been requested:')
+    n = 1
     for topping in toppings:
-                print(f'- {topping}')
-make_pizza('pepperoni')
-make_pizza('cheese', 'tomatoes', 'rukkola')
+                print(f'{n}-size-{size} - {topping}')
+                n = n + 1
+make_pizza(16, 'pepperoni')
+make_pizza(32, 'cheese', 'tomatoes', 'rukkola')
