0d673860acccaa438274e6518761278caa62edb8
neuro <yamsh@yandex.ru>
2023-08-22 18:24:48 +0600

    technical commit

--- pizza.py
+++ pizza.py
@@ -1,7 +1,7 @@
-def make_pizza(*toppings):
+def make_pizza(size, *toppings):
     """Print a list of toppings that have been requested"""
     print(f'Here is the list of the toppings have been requested:')
     for topping in toppings:
-                print(f'- {topping}')
-make_pizza('pepperoni')
-make_pizza('cheese', 'tomatoes', 'rukkola')
+                print(f' size - {size} toppings - {topping}')
+make_pizza('21"', 'pepperoni')
+make_pizza('32"','cheese', 'tomatoes', 'rukkola')
