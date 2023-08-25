d18a2cc1103cf16842e2c9affb066f390fcab05b
neuro <yamsh@yandex.ru>
2023-08-25 18:21:42 +0600

    saving pizza.py as module

--- pizza.py
+++ pizza.py
@@ -1,15 +1,7 @@
 def make_pizza(size, *toppings):
     """Print a list of toppings that have been requested"""
-    print(f'Here is the list of the toppings have been requested:')
-    n = 1
+    print(f'Making a {size}-inch pizza with the following topings:')
+
     for topping in toppings:
-<<<<<<< HEAD
-                print(f' size - {size} toppings - {topping}')
-make_pizza('21"', 'pepperoni')
-make_pizza('32"','cheese', 'tomatoes', 'rukkola')
-=======
-                print(f'{n}-size-{size} - {topping}')
-                n = n + 1
-make_pizza(16, 'pepperoni')
-make_pizza(32, 'cheese', 'tomatoes', 'rukkola')
->>>>>>> origin/master
+        print(f'toppings - {topping}')
+
