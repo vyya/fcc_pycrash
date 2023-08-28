01f137b0e48f1c58ca12fdb089f5ee04dfc22fe6
neuro <yamsh@yandex.ru>
2023-08-28 17:17:55 +0600

    import module 'sandwiches' to print data about sandwiches

---
+++ sandwich_print.py
@@ -0,0 +1,6 @@
+from sandwiches import make_sandwich as ms
+
+
+ms('hamburger', 'dough', 'beef', 'cucumber', 'salad')
+ms('cheesburger', 'chees', 'chicken', 'tomato','cabbage')
+ms('pita', 'minced meat', 'onion', 'garlic')
