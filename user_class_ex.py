35c9497f0a9833075565d09c484f03dd91e1a865
neuro <yamsh@yandex.ru>
2023-08-30 16:17:32 +0600

    login_attempts attribute added and changing through the methods - increment, reset

--- user_class_ex.py
+++ user_class_ex.py
@@ -5,19 +5,30 @@ class User:
         self.gender = gender
         self.age = age
         self.user_id = user_id
+        self.login_attempts = 0
 
     def describe_user(self):
         print(f"Here is the {self.age} years old  user {self.first_name} {self.last_name} with user id {self.user_id}")
 
     def greet_user(self):
         if self.gender == 'male':
-            print(f"You're welcome Mr {self.first_name} {self.last_name}")
+            print(f"You're welcome Mr {self.first_name} {self.last_name}"
+                  f" You have a {self.login_attempts} login attempts")
         else:
-            print(f"You're welcome Miss {self.first_name} {self.last_name}")
+            print(f"You're welcome Miss {self.first_name} {self.last_name}"
+                  f" You have a {self.login_attempts} login attempts")
+    def increment_login_attempts(self):
+        self.login_attempts += 1
+    def reset_login_attemps(self):
+        self.login_attempts = 0
 
 sara = User('Sara', 'Biscuit', 'female', 23, 755)
 john = User('John', 'Milligan', 'male', 42, 343)
 gina = User('Gina', 'Palmer', 'female', 31, 451)
+sara.increment_login_attempts()
+john.increment_login_attempts()
+gina.increment_login_attempts()
+gina.reset_login_attemps()
 sara.describe_user()
 sara.greet_user()
 gina.describe_user()
