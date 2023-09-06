b861b08770a0020d7d4babe696c8dfb4fc10acdf
neuro <yamsh@yandex.ru>
2023-09-06 20:27:26 +0600

    importing module into a module

--- car_import_class/electric_car.py
+++ car_import_class/electric_car.py
@@ -1,33 +1,7 @@
-"""A class that can be used to represent a car"""
-class Car:
-    """An attempt to represent a car"""
-    def __init__(self, make, model, year):
-        """Initialize attributes to describe a car"""
-        self.make = make
-        self.model = model
-        self.year = year
-        self.odometer_reading = 0
-        self.gas_tank_level = 20
+from car import Car
 
-    def get_descriptive_name(self):
-        long_name = f"{self.year} {self.make} {self.model}"
-        return long_name.title()
 
-    def read_odometer(self):
-        """Print a statement showing car's milage"""
-        print(f'This car has a {self.odometer_reading} miles on it')
-
-    def update_odometer(self, mileage):
-        """Set odometer reading to the given value."""
-        if mileage >= self.odometer_reading:
-            self.odometer_reading = mileage
-        else:
-            print('You can\'t roll back an odometer')
-
-    def increment_odometer(self, miles):
-        self.odometer_reading += miles
-
-''' class Battery:
+class Battery:
     """An attempt to model a battery for an electric car"""
     def __init__(self, battery_size = 75 ):
         """Initialize the batterrie's attribute"""
@@ -56,4 +30,3 @@ class ElectricCar(Car):
     def fill_gas_tank(self):
         """Electric cars don't have gas tank"""
         print('This car doesn\'t have a gas tank!')
-'''
--- car_import_class/my_cars.py
+++ car_import_class/my_cars.py
@@ -1,4 +1,4 @@
-# from car import Car, ElectricCar
+'''# from car import Car, ElectricCar
 import car
 
 # my_nissan = Car('Nissan', 'Terrano', 2008)
@@ -6,4 +6,13 @@ my_nissan = car.Car('Nissan', 'Terrano', 2008)
 print(my_nissan.get_descriptive_name())
 # my_tesla = ElectricCar('Tesla', 'Lotus', 2019)
 my_tesla = car.ElectricCar('Tesla', 'Lotus', 2019)
-print(my_tesla.get_descriptive_name())
+print(my_tesla.get_descriptive_name())'''
+
+from car import Car
+from electric_car import ElectricCar
+
+my_leaf = Car('nissan', 'leaf', 2022)
+print(my_leaf.get_descriptive_name())
+
+my_zeekr = ElectricCar('zeekr', 'bomb', 2021)
+print(my_zeekr.get_descriptive_name())
