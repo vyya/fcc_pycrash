957f08e20d25e0a2222e0b713979a08622c74b2d
neuro <yamsh@yandex.ru>
2023-09-06 18:12:30 +0600

    importing more classes from the module car.py

---
+++ car_import_class/my_electric_car.py
@@ -0,0 +1,6 @@
+from car import ElectricCar
+
+my_electric = ElectricCar('Zeekr', 'Instant', 2019)
+my_electric.fill_gas_tank()
+my_electric.battery.describe_battery()
+my_electric.battery.get_range()
