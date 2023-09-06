930d5846f8969a71b7aaf324dfa96a1d0f703fdd
neuro <yamsh@yandex.ru>
2023-09-06 18:25:19 +0600

    import multiple classes from a module

---
+++ car_import_class/my_cars.py
@@ -0,0 +1,6 @@
+from car import Car, ElectricCar
+
+my_nissan = Car('Nissan', 'Terrano', 2008)
+print(my_nissan.get_descriptive_name())
+my_tesla = ElectricCar('Tesla', 'Lotus', 2019)
+print(my_tesla.get_descriptive_name())
