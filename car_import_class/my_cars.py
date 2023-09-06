721807c0b217579dc1de0e7502d942ca4ee7d72a
neuro <yamsh@yandex.ru>
2023-09-06 18:34:04 +0600

    importing an entire module car

--- car_import_class/my_cars.py
+++ car_import_class/my_cars.py
@@ -1,6 +1,9 @@
-from car import Car, ElectricCar
+# from car import Car, ElectricCar
+import car
 
-my_nissan = Car('Nissan', 'Terrano', 2008)
+# my_nissan = Car('Nissan', 'Terrano', 2008)
+my_nissan = car.Car('Nissan', 'Terrano', 2008)
 print(my_nissan.get_descriptive_name())
-my_tesla = ElectricCar('Tesla', 'Lotus', 2019)
+# my_tesla = ElectricCar('Tesla', 'Lotus', 2019)
+my_tesla = car.ElectricCar('Tesla', 'Lotus', 2019)
 print(my_tesla.get_descriptive_name())
