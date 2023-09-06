298c303b8db5a5fcbbe957c5f8b4c9f5d54ae07f
neuro <yamsh@yandex.ru>
2023-09-06 18:18:14 +0600

    method calling has been modified

--- car_import_class/my_car.py
+++ car_import_class/my_car.py
@@ -1,7 +1,6 @@
 from car import Car
 
 my_new_car = Car('Subaru', 'Forester', 2007)
-desc_name = my_new_car.get_descriptive_name()
-print(desc_name)
+print(my_new_car.get_descriptive_name())
 my_new_car.odometer_reading = 79
 my_new_car.read_odometer()
