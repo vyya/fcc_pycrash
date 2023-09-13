ae48f44d09eae84f62d86b81e050cecad7280591
neuro <yamsh@yandex.ru>
2023-09-13 17:54:26 +0600

    read long txt and generate string with fixed width

--- ch10_files/pi_string.py
+++ ch10_files/pi_string.py
@@ -1,11 +1,11 @@
 from pathlib import Path
 
-path = Path('pi_digits.txt')
+path = Path('pi_million_digits.txt')
 content = path.read_text()
 lines = content.splitlines()
-print(lines)
+
 pi_string = ''
 for line in lines:
     pi_string += line.lstrip()
-print(pi_string)
+print(f'{pi_string[:52]}')
 print(len(pi_string))
