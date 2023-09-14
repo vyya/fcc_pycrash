b957e5d1d2b4fd0bd163a6a9dce7152f9faad81d
neuro <yamsh@yandex.ru>
2023-09-14 14:17:23 +0600

    analyze content of the text file

--- ch10_files/pi_string.py
+++ ch10_files/pi_string.py
@@ -7,5 +7,10 @@ lines = content.splitlines()
 pi_string = ''
 for line in lines:
     pi_string += line.lstrip()
+birthday = input('Please input your birthday in format "ddmmyy":')
+if birthday in pi_string:
+    print(f'\nYour birthday {birthday} appear in the pi digit\n')
+else:
+    print(f'Your birthday doesn\'t appear in the pi digit')
 print(f'{pi_string[:52]}')
 print(len(pi_string))
