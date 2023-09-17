a5755fee8c8555b920f657717e5a39c2a2b453d1
neuro <yamsh@yandex.ru>
2023-09-17 19:43:17 +0600

    pathlib -passing a list, silently failed except block

--- ch10_files/alice.py
+++ ch10_files/alice.py
@@ -1,14 +1,29 @@
 from pathlib import Path
 
-path = Path('alice.txt')
-try:
-    content = path.read_text(encoding = 'utf-8')
-except:
-    print(f'The file {path} does not exist. Enter a valid path')
-
-else:
-    # Counts the approximate number of words in the file
-    words = content.split()
+def count_words(path):
+                               
     
-    num_words = len(words)
-    print(f'The file {path} has about {num_words} words.')
+        try:
+            content = path.read_text(encoding = 'utf-8')
+        except:
+
+            # print(f'The file {path} does not exist. Enter a valid path')
+            pass
+
+        else:
+            # Counts the approximate number of words in the file
+            words = content.split()
+            
+            num_words = len(words)
+            print(f'The file {path} has about {num_words} words.')
+
+filenames = [
+            'alice.txt', 
+            'sidhartha.txt',
+            'moby_dick.txt',
+            'little_women.txt'            
+            ]
+
+for filename in filenames:
+    path = Path(filename)
+    count_words(path)
