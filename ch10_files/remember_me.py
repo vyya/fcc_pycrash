a9268aa2c5933335d94700b276cafcc7311be792
neuro <yamsh@yandex.ru>
2023-09-29 14:17:12 +0600

    compartmentalization by different functions

--- ch10_files/remember_me.py
+++ ch10_files/remember_me.py
@@ -1,8 +1,31 @@
 from pathlib import Path
 import json
 
-username = input('Please enter your username: ')
-path = Path('username.json')
-content = json.dumps(username)
-path.write_text(content)
-print(f'We\'ll remember you when you comes back next time {username}')
+def get_stored_username(path):
+    """ Get stored username if available """
+    if path.exists():
+        contents = path.read_text()
+        username = json.loads(contents)
+        return username
+    else:
+        return None
+
+
+def get_new_username(path):
+    """ Prompt new username"""
+    username = input("Pleease enter your username: ")
+    contents = json.dumps(username)
+    path.write_text(contents)
+
+
+def greet_user():
+    """ Greet the user by name """
+    path = Path('username.json')
+    username = get_stored_username(path)
+    if username:
+        print(f'Nice to see you again here {username}!')
+    else:
+        username = get_new_username(path)
+        print(f'We\'ll remember you when you comes back next time {username}')
+
+greet_user()
