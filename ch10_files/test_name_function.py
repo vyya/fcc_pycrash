a8b628042010bd065e62177ccff52d4e6e65c809
neuro <yamsh@yandex.ru>
2023-10-29 22:39:26 +0600

    new test with middle name added

--- ch10_files/test_name_function.py
+++ ch10_files/test_name_function.py
@@ -1,6 +1,11 @@
 from name_function import get_formatted_name
 
 def test_first_last_name():
-    """ Do names like 'Janis Joplin work? """
-    formatted_name = get_formatted_name('janis', 'joplin')
-    assert formatted_name == 'Janis Joplin'
+    """Do names like 'Lynda Murrey' work """
+    formatted_name = get_formatted_name('lynda', 'murrey')
+    assert formatted_name == 'Lynda Murrey'
+
+def test_first_last_middle_name():
+    """ Do names like 'Wolfgang Amadeus Mozart' work? """
+    formatted_name = get_formatted_name('wolfgang', 'mozart', 'amadeus')
+    assert formatted_name == 'Wolfgang Amadeus Mozart'
