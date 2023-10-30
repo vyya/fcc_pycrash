8fdda9bdce2ed29847e10100784c9797d5990d23
neuro <yamsh@yandex.ru>
2023-10-31 00:27:10 +0600

    essential indents added

--- ch11_files/survey.py
+++ ch11_files/survey.py
@@ -1,21 +1,21 @@
 class AnonymousSurvey:
     """ Collect anonymous answers to a survey questions. """
 
-def __init__(self, question):
-    """ Store a question, and prepare to store response """
-    self.question = question
-    self.responses = []
+    def __init__(self, question):
+        """ Store a question, and prepare to store response """
+        self.question = question
+        self.responses = []
 
-def show_question(self):
-    """ Show the survey question """
-    print(self.question)
+    def show_question(self):
+        """ Show the survey question """
+        print(self.question)
 
-def store_response(self, new_response):
-    """ Store a single response to the survey """
-    self.responses.append(new_response)
+    def store_response(self, new_response):
+        """ Store a single response to the survey """
+        self.responses.append(new_response)
 
-def show_results(self):
-    """ Show all responses that have been given """
-    print("Survey results: \n")
-    for response in self.responses:
-        print(f'- {response}')
+    def show_results(self):
+        """ Show all responses that have been given """
+        print("Survey results: \n")
+        for response in self.responses:
+            print(f'- {response}')
