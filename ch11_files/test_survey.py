40d8c8d482260f0f64837ce8942499a88b68deb7
neuro <yamsh@yandex.ru>
2023-11-01 00:54:59 +0600

    another test for multiple responses added

--- ch11_files/test_survey.py
+++ ch11_files/test_survey.py
@@ -6,3 +6,14 @@ def test_store_single_response():
     language_survey = AnonymousSurvey(question)
     language_survey.store_response('English')
     assert 'English' in language_survey.responses
+
+def test_store_three_responses():
+    """ Test that three individual responses are stored properly """
+    question = "What language did you learn first to speak?"
+    language_survey = AnonymousSurvey(question)
+    responses = ['English', 'Italian', 'French']
+    for response in responses:
+        language_survey.store_response(response)
+    
+    for response in responses:
+        assert response in language_survey.responses
